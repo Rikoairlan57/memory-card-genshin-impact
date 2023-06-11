e1747f4362fe6a1344207fea631e30e3cd875f07
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-06-11 10:12:56 +0700

    ✨ 🎨 fundamental layout

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+interface MainProps {
+  children: React.ReactNode;
+}
+
+export function Main({ children }: MainProps) {
+  return (
+    <main className="flex w-full max-w-7xl flex-1 items-center justify-center gap-8 rounded-lg p-2 py-6 sm:p-8">
+      {children}
+    </main>
+  );
+}
