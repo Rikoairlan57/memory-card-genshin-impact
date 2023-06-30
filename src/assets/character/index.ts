a0ea3e94b8d93beb800dca507d80c8bde1293f30
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-06-30 14:53:47 +0700

    🎨 ⚡️ fixed export global assets

---
+++ src/assets/character/index.ts
@@ -0,0 +1,141 @@
+import raidenShogun from "./Raiden-Shogun.jpeg";
+import klee from "./Klee.png";
+import ayaka from "./Ayaka.png";
+import amber from "./Amber.png";
+import aether from "./Aether.jpeg";
+import albedo from "./albedo.jpeg";
+import alhaitham from "./Alhaitham.png";
+import arataki from "./Arataki.jpeg";
+import ayato from "./Ayato.jpeg";
+import baizhu from "./Baizhu.jpeg";
+import barbara from "./Barbara.jpeg";
+import beidou from "./Beidou.jpeg";
+import bennett from "./Bennett.jpeg";
+import candace from "./Candace.png";
+import chongyun from "./Chongyun.jpeg";
+import collei from "./Collei.jpeg";
+import cyno from "./Cyno.jpeg";
+import dehya from "./Dehya.jpeg";
+import diluc from "./Diluc.jpeg";
+import diona from "./Diona.png";
+import dori from "./Dori.jpeg";
+import eula from "./Eula.png";
+import faruzan from "./Faruzan.jpeg";
+import fischl from "./Fischl.jpeg";
+import ganyu from "./Ganyu.jpeg";
+import gorou from "./Gorou.png";
+import hutao from "./HuTao.png";
+import jean from "./Jean.jpeg";
+import kaeya from "./Kaeya.png";
+import kaveh from "./Kaveh.jpeg";
+import kazuha from "./Kazuha.png";
+import keqing from "./Keqing.jpeg";
+import kirara from "./Kirara.jpeg";
+import kokomi from "./Kokomi.jpeg";
+import kujouSara from "./Kujou-Sara.png";
+import kukiShinobu from "./Kuki-Shinobu.png";
+import layla from "./Layla.png";
+import lisa from "./Lisa.jpeg";
+import lumine from "./Lumine.jpeg";
+import mika from "./Mika.png";
+import mona from "./Mona.jpeg";
+import nahida from "./Nahida.png";
+import nilou from "./Nilou.png";
+import ningguang from "./Ningguang.jpeg";
+import noelle from "./Noelle.png";
+import paimon from "./Paimon.jpeg";
+import qiqi from "./Qiqi.png";
+import razor from "./Razor.jpeg";
+import rosaria from "./Rosaria.jpeg";
+import sayu from "./Sayu.png";
+import shenhe from "./Shenhe.png";
+import shikanoinHeizou from "./Shikanoin-Heizou.jpeg";
+import sucrose from "./Sucrose.png";
+import tartaglia from "./Tartaglia.jpeg";
+import thoma from "./Thoma.jpeg";
+import tighnari from "./Tighnari.png";
+import venti from "./Venti.jpeg";
+import wanderer from "./Wanderer.jpeg";
+import xiangling from "./Xiangling.jpeg";
+import xiao from "./Xiao.jpeg";
+import xingqiu from "./Xingqiu.jpeg";
+import xinyan from "./Xinyan.png";
+import yaeMiko from "./Yae-Miko.jpeg";
+import yanfei from "./Yanfei.png";
+import yaoyao from "./Yaoyao.jpeg";
+import yelan from "./Yelan.png";
+import yoimiya from "./Yoimiya.jpeg";
+import yunjin from "./Yun-Jin.jpeg";
+import zhongli from "./Zhongli.jpeg";
+
+export {
+  faruzan,
+  ganyu,
+  fischl,
+  gorou,
+  hutao,
+  raidenShogun,
+  klee,
+  ayaka,
+  amber,
+  aether,
+  albedo,
+  alhaitham,
+  arataki,
+  ayato,
+  baizhu,
+  beidou,
+  bennett,
+  barbara,
+  candace,
+  chongyun,
+  collei,
+  cyno,
+  dehya,
+  diluc,
+  diona,
+  dori,
+  eula,
+  jean,
+  kaeya,
+  kaveh,
+  kazuha,
+  keqing,
+  kirara,
+  kokomi,
+  kujouSara,
+  kukiShinobu,
+  layla,
+  lisa,
+  lumine,
+  mika,
+  mona,
+  nahida,
+  nilou,
+  ningguang,
+  noelle,
+  paimon,
+  qiqi,
+  razor,
+  rosaria,
+  sayu,
+  shenhe,
+  shikanoinHeizou,
+  sucrose,
+  tartaglia,
+  thoma,
+  tighnari,
+  venti,
+  wanderer,
+  xiangling,
+  xiao,
+  xingqiu,
+  xinyan,
+  yaeMiko,
+  yanfei,
+  yaoyao,
+  yelan,
+  yoimiya,
+  yunjin,
+  zhongli,
+};
