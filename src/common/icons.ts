0d4a752183a9bbb6a68012c58fd653a308939392
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-06-30 16:08:41 +0700

    ⚡️ all types card component

---
+++ src/common/icons.ts
@@ -0,0 +1,3 @@
+export { VscLoading, VscError } from "react-icons/vsc";
+export { GiPartyPopper } from "react-icons/gi";
+export { FaSkull } from "react-icons/fa";
---
+++ src/types/ICurrentCards.ts
@@ -0,0 +1,6 @@
+import type { ICard } from "./ICard";
+
+export type ICurrentCards =
+  | [ICard, ICard, ICard]
+  | [ICard, ICard, ICard, ICard]
+  | [ICard, ICard, ICard, ICard, ICard];
